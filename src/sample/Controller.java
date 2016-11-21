a883a818ccfaf2cee5ecb3bae2e646ed4172a1db
marcoscanton <46453895j@iespoblenou.org>
2016-11-21 20:29:35 +0100

    seleccion de detalle carta

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,28 @@
 package sample;
 
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.ImageViewBuilder;
 import javafx.util.Callback;
 
 import java.util.ArrayList;
 
+
+
 public class Controller {
+
+    public TextArea Tadescripcion;
+    public ImageView Ivdescripcion;
 
     @FXML
     ListView<Ocarta> Lvcartas ;
@@ -42,5 +53,20 @@ public class Controller {
         ObservableList<Ocarta> items = FXCollections.observableArrayList (
              listaocartas);
         Lvcartas.setItems(items);
+
+
+        Lvcartas.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Ocarta>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Ocarta> observable, Ocarta oldValue, Ocarta newValue) {
+
+                        //System.out.println(newValue.getTitulo());
+                        Tadescripcion.setText(newValue.getDescripcion());
+                        Image image1 = new Image(newValue.getImageUrl());
+                        Ivdescripcion.setImage(image1);
+
+                    }
+        });
+
     }
+
 }
